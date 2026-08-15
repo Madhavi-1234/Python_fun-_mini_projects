04e7b697896a65ab22b54d8d5ccf5dd922d03d21
Madhavi <dixitmadhavi12@gmail.com>
2026-08-15 16:00:10 +0530

    Add function to find second largest element
    Implement function to find the second largest element in a list.

--- Functions/Second_largest_elemnt.py
+++ Functions/Second_largest_elemnt.py
@@ -1,1 +1,16 @@
+## second Largest Element
 
+def secondlargest(num):
+  largest= num[0]
+  second= None
+  for i in range(len(num)):
+    if num[i]> largest:
+      largest= num[i]
+  for i in range(len(num)):
+    if num[i]< largest:
+      if second is None or num[i]>second:
+       second= num[i]
+  return second
+
+num= [1,44,5,66,3,44,88,9]
+print(secondlargest(num))
