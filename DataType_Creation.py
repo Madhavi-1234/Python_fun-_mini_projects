bf467fb0f1841e8ee124f1daf7b1a81a38832559
Madhavi <dixitmadhavi12@gmail.com>
2026-08-25 18:48:39 +0530

    Update DataType_Creation.py

--- DataType_Creation.py
+++ DataType_Creation.py
@@ -1,1 +1,14 @@
+## data type creation
 
+class Fraction:
+
+ def __init__(self, n, d):
+  self.num= n
+  self.den= d
+
+ def __str__(self):
+  return "{}\{}".format(self.num, self.den)
+
+
+frac= Fraction(3,5)
+print(frac)
